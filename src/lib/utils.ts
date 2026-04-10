a1928350b21b031fdce4e016b80b00b31712949b
nielskrikke <nielskrikkeon@gmail.com>
2026-04-10 20:40:01 +0200

    refactor: Remove `clsx` and `tailwind-merge` dependencies
    The utility function `cn` has been reimplemented internally to achieve the same functionality as `clsx` and `tailwind-merge`, allowing for the removal of these external dependencies. This reduces the project's bundle size and simplifies dependency management.
    
    Additionally, the Vite server configuration has been updated to use port 3000 and host 0.0.0.0 for broader accessibility. The `tailwindcss` Vite plugin has also been correctly included.

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,40 @@
-import { clsx, type ClassValue } from 'clsx';
-import { twMerge } from 'tailwind-merge';
+type ClassValue = string | number | boolean | undefined | null | { [key: string]: any } | ClassValue[];
+
+function toVal(mix: any) {
+  let k, y, str = '';
+  if (typeof mix === 'string' || typeof mix === 'number') {
+    str += mix;
+  } else if (typeof mix === 'object') {
+    if (Array.isArray(mix)) {
+      for (k = 0; k < mix.length; k++) {
+        if (mix[k]) {
+          if (y = toVal(mix[k])) {
+            str && (str += ' ');
+            str += y;
+          }
+        }
+      }
+    } else {
+      for (k in mix) {
+        if (mix[k]) {
+          str && (str += ' ');
+          str += k;
+        }
+      }
+    }
+  }
+  return str;
+}
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs));
+  let i = 0, tmp, x, str = '';
+  while (i < inputs.length) {
+    if (tmp = inputs[i++]) {
+      if (x = toVal(tmp)) {
+        str && (str += ' ');
+        str += x;
+      }
+    }
+  }
+  return str;
 }
